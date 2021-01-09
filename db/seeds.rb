70fda9f2ef02db80044286ee522432d51477f798
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-01-08 21:04:10 -0800

    adding seed files to test database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+vendor1 = Vendor.create(name: "Costco", total_cost: 74.63)
+product1 = Product.create(vendor_id: 1, name: "Brorganics - 1 gallon milk", description: "1 gallon organic milk", price: 10.00, kind:"add" )
+product2 = Product.create(vendor_id: 1, name: "Safechipz - Large potato chips", description: "1 bag of organic chips", price: 8.00, kind:"delete")
