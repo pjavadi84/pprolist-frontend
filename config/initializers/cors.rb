a8b668c3656182fb4e5ed2e5e8904c926f3cddff
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-02-20 16:37:17 -0800

    updating CORS origins to heroku server

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://polar-refuge-23156.herokuapp.com/'
 
     resource '*',
       headers: :any,
