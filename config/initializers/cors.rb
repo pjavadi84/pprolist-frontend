7df972145612064b9cf942f4dccf594efcdb93f6
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-02-20 17:04:17 -0800

    reverted CORS origins to *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://polar-refuge-23156.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
